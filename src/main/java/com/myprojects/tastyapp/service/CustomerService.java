3f1c48c2767b9c79957b799231aeb51703cdd880
TheOldEnglishSheepdog <58194727+TheOldEnglishSheepdog@users.noreply.github.com>
2022-07-26 16:37:58 -0700

    CustomerService

--- src/main/java/com/myprojects/tastyapp/service/CustomerService.java
+++ src/main/java/com/myprojects/tastyapp/service/CustomerService.java
@@ -1,6 +1,7 @@
 package com.myprojects.tastyapp.service;
 
 import com.myprojects.tastyapp.dao.CustomerDao;
+import com.myprojects.tastyapp.entity.Cart;
 import com.myprojects.tastyapp.entity.Customer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -11,8 +12,17 @@ public class CustomerService {
     @Autowired
     public CustomerDao customerDao;
 
+    /**
+    * 1. assign a cart to the new customer
+     * 2. set enable
+     * 3. save customer to db
+    * */
     public void signUp(Customer customer) {
+        Cart cart = new Cart();
+        customer.setCart(cart);
 
+        customer.setEnable(true);
+        customerDao.signUp(customer);
     }
 
     public Customer getCustomer(String email) {
