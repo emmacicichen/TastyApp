4a93da1b7b96792797ba8247f101a3d281e26a51
TheOldEnglishSheepdog <58194727+TheOldEnglishSheepdog@users.noreply.github.com>
2022-07-26 16:35:11 -0700

    CustomerDao

--- src/main/java/com/myprojects/tastyapp/ApplicationConfig.java
+++ src/main/java/com/myprojects/tastyapp/ApplicationConfig.java
@@ -7,6 +7,7 @@ import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 import javax.sql.DataSource;
+import java.util.Properties;
 
 @Configuration
 @EnableWebMvc
@@ -17,7 +18,7 @@ public class ApplicationConfig {
         LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
         sessionFactory.setDataSource(dataSource());
         sessionFactory.setPackagesToScan("com.myprojects.tastyapp.entity");
-        //sessionFactory.setHibernateProperties(hibernateProperties());
+        sessionFactory.setHibernateProperties(hibernateProperties());
         return sessionFactory;
     }
 
@@ -25,12 +26,21 @@ public class ApplicationConfig {
     public DataSource dataSource() {
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
         dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
-        dataSource.setUrl("jdbc:mysql://YOUR_RDS_INSTANCE_ADDRESS:3306/onlineOrder?createDatabaseIfNotExist=true&serverTimezone=UTC");
-        dataSource.setUsername("USERNAME");
-        dataSource.setPassword("PASSWORD");
+        dataSource.setUrl("jdbc:mysql://tastyapp-instance.ccpe5uo60zwd.us-east-1.rds.amazonaws.com:3306/tastyapp?createDatabaseIfNotExist=true&serverTimezone=UTC");
+        dataSource.setUsername("admin");
+        dataSource.setPassword("tastyapp");
 
         return dataSource;
     }
+
+    private final Properties hibernateProperties() {
+        Properties hibernateProperties = new Properties();
+        hibernateProperties.setProperty("hibernate.hbm2ddl.auto", "update");
+        hibernateProperties.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQL5InnoDBDialect");
+        hibernateProperties.setProperty("hibernate.show_sql", "true");
+        return hibernateProperties;
+    }
+
 }
 
 
--- src/main/java/com/myprojects/tastyapp/dao/CustomerDao.java
+++ src/main/java/com/myprojects/tastyapp/dao/CustomerDao.java
@@ -1,16 +1,56 @@
 package com.myprojects.tastyapp.dao;
 
+import com.myprojects.tastyapp.entity.Authorities;
 import com.myprojects.tastyapp.entity.Customer;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class CustomerDao {
+    @Autowired
+    private SessionFactory sessionFactory;
+
+    /*
+    * 1. assign an authority for the new customer
+    * 2. get session from SessionFactory
+    * 3. define unit of work by Transaction
+    * 4. save in db
+    * */
     public void signUp(Customer customer) {
+        Authorities authorities = new Authorities();
+        authorities.setAuthorities("ROLE_USER");
+        authorities.setEmail(customer.getEmail());
+
+        Session session = null;
+        try{
+            session = sessionFactory.openSession();
+            session.beginTransaction();
+            session.save(authorities);
+            session.save(customer);
+            session.getTransaction().commit();
+
+        } catch(Exception ex) {
+            ex.printStackTrace();
+            session.getTransaction().rollback();
+
+        } finally {
+            if(session != null) {
+                session.close();
+            }
+        }
 
     }
 
     public Customer getCustomer(String email) {
-        return new Customer();
+        Customer customer = null;
+        try(Session session = sessionFactory.openSession()) {
+            customer = session.get(Customer.class, email);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        return customer;
     }
 
 }
